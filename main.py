c11b081845ec1bc96d1b44560f9b8f9e6eb3ce8a
kvms-lokesh <lokesh@merklescience.com>
2026-05-08 22:26:46 +0530

    v2

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-        return 'Konnichiwa for GKE'
+        return 'Konnichiwa for GKE v2.0'
 if __name__=='__main__':
         app.run(host='0.0.0.0',port=8080)
